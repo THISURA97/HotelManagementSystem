655e2a604d5ded573a80021fc6ccceecba7c1b31
Thisura Hettiarachchi <38825184+THISURA97@users.noreply.github.com>
2019-07-20 09:40:16 +0530

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+        <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+@section('content')
+
+
+
+</body>
+</html>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,61 @@
 @extends('layout')
 
 @section('title','Home page')
-@section('content')
-    <h1>Welcome</h1>
-    @endsection
+@section('carousel1')
+
+        <!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+</head>
+<body>
+<div id="demo" class="carousel slide carousel overflow-hidden" >
+
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner" >
+            <div class="carousel-item active">
+                <img src="https://refreshhikkaduwa.com/images/REFRESH/Main-slider/refresh-deluxe-new.jpg" alt="Los Angeles">
+                <div class="carousel-caption">
+                    <h3>Los Angeles</h3>
+                    <p>We had such a great time in LA!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://refreshhikkaduwa.com/images/REFRESH/Main-slider/refresh-deluxe-new.jpg" alt="Chicago" >
+                <div class="carousel-caption">
+                    <h3>Chicago</h3>
+                    <p>Thank you, Chicago!</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://refreshhikkaduwa.com/images/REFRESH/Main-slider/refresh-deluxe-new.jpg" alt="New York" >
+                <div class="carousel-caption">
+                    <h3>New York</h3>
+                    <p>We love the Big Apple!</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+</div>
+
+</body>
+</html>
+@endsection
+@section('carousel2')
+
+
+
+@endsection
