0fc1b8e1fca10429eff6b44b48cc858d98028257
Thisura Hettiarachchi <38825184+THISURA97@users.noreply.github.com>
2019-07-23 09:22:46 +0530

    Add files via upload

---
+++ database/migrations/2019_07_21_034735_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('email');
+            $table->string('phoneNumber');
+            $table->string('date');
+            $table->string('month');
+            $table->string('year');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
